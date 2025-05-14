0564076ce76f5da887986031583cf8b507e09aac
Ax-47 <armingLwnza@gmail.com>
2025-05-14 19:03:06 +0700

    add: sql

---
+++ database/get_username.sql
@@ -0,0 +1,5 @@
+select username from axcelium.users;
+select username from axcelium.users_by_email;
+select username from axcelium.users_by_username;
+select username from axcelium.user_organizations;
+select username from axcelium.user_organizations_by_user;
