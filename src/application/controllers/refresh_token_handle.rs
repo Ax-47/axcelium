f644856c4a61873857ac092501b49d0cb3c0c76f
Ax-47 <armingLwnza@gmail.com>
2025-05-20 21:38:49 +0700

    นอนดีกว่า

--- src/application/controllers/refresh_token_handle.rs
+++ src/application/controllers/refresh_token_handle.rs
@@ -1,4 +1,5 @@
-use super::super::services::users::create::CreateUserService;
+use crate::application::dto::payload::refresh_token::GetUserIdQuery;
+use crate::application::services::refresh_token::create::CreateRefreshTokenService;
 use crate::domain::{
     entities::apporg_client_id::CleanAppOrgByClientId, errors::repositories_errors::ApiError,
 };
@@ -6,13 +7,14 @@ use actix_web::HttpMessage;
 use actix_web::{Result, web};
 pub async fn create_refresh_token_handle(
     req: actix_web::HttpRequest,
-    user_service: web::Data<dyn CreateUserService>,
-    post_data: web::Json<()>,
+    path: web::Path<GetUserIdQuery>,
+    token_service: web::Data<dyn CreateRefreshTokenService>,
 ) -> Result<web::Json<()>, ApiError> {
     let apporg = req
         .extensions()
         .get::<CleanAppOrgByClientId>()
         .ok_or_else(|| ApiError::new("Missing AppOrg data".to_string(), 500))
         .cloned()?;
+    token_service.execute().await;
     Ok(web::Json(()))
 }
