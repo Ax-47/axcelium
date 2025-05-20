c5c017413a564bd0dd95bac4d4b1cf573d737f39
Ax-47 <armingLwnza@gmail.com>
2025-05-21 00:33:37 +0700

    fix: database repo

--- src/infrastructure/repositories/cache_layer/refresh_token_repository.rs
+++ src/infrastructure/repositories/cache_layer/refresh_token_repository.rs
@@ -1,19 +1,18 @@
 use crate::domain::errors::repositories_errors::RepositoryResult;
-use crate::infrastructure::models::apporg_client_id::AppOrgModel;
+use crate::infrastructure::models::refresh_token::RefreshTokenModel;
 use crate::infrastructure::repositories::cache::applications_organization_by_client_id_repository::ApplicationsOrganizationByClientIdCacheRepository;
-use crate::infrastructure::repositories::database::applications_organization_by_client_id_repository::ApplicationsOrganizationByClientIdDatabaseRepository;
+use crate::infrastructure::repositories::database::refresh_token::RefreshTokenDatabaseRepository;
 use async_trait::async_trait;
 use std::sync::Arc;
-use uuid::Uuid;
 pub struct RefreshTokenCacheLayerRepositoryImpl {
     cache_repo: Arc<dyn ApplicationsOrganizationByClientIdCacheRepository>,
-    database_repo: Arc<dyn ApplicationsOrganizationByClientIdDatabaseRepository>,
+    database_repo: Arc<dyn RefreshTokenDatabaseRepository>,
 }
 
 impl RefreshTokenCacheLayerRepositoryImpl {
     pub fn new(
         cache_repo: Arc<dyn ApplicationsOrganizationByClientIdCacheRepository>,
-        database_repo: Arc<dyn ApplicationsOrganizationByClientIdDatabaseRepository>,
+        database_repo: Arc<dyn RefreshTokenDatabaseRepository>,
     ) -> Self {
         Self {
             cache_repo,
@@ -23,29 +22,15 @@ impl RefreshTokenCacheLayerRepositoryImpl {
 }
 #[async_trait]
 pub trait RefreshTokenCacheLayerRepository: Send + Sync {
-    async fn find_apporg_by_client_id(
-        &self,
-        client_id: Uuid,
-    ) -> RepositoryResult<Option<AppOrgModel>>;
-    async fn create_apporg_by_client_id(&self, apporg: AppOrgModel) -> RepositoryResult<()>;
+    async fn create_refresh_token(&self, rt: RefreshTokenModel) -> RepositoryResult<()>;
 }
 
 #[async_trait]
 impl RefreshTokenCacheLayerRepository
     for RefreshTokenCacheLayerRepositoryImpl
 {
-    async fn find_apporg_by_client_id(&self, key: Uuid) -> RepositoryResult<Option<AppOrgModel>> {
-        if let Some(cached_data) = self.cache_repo.get_cached_apporg_by_client_id(key).await? {
-            return Ok(Some(cached_data));
-        }
-        let Some(data) = self.database_repo.find_apporg_by_client_id(key).await? else {
-            return Ok(None);
-        };
-        self.cache_repo.cache_apporg_by_client_id(&data).await?;
-        Ok(Some(data))
-    }
 
-    async fn create_apporg_by_client_id(&self, apporg: AppOrgModel) -> RepositoryResult<()> {
-        self.database_repo.create_apporg_by_client_id(apporg).await
+    async fn create_refresh_token(&self, rt: RefreshTokenModel) -> RepositoryResult<()> {
+        self.database_repo.create_refresh_token(rt).await
     }
 }
