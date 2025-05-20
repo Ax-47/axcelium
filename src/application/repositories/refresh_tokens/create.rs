dd6df10466fcbf24c25c78dde729d98d070a3428
Ax-47 <armingLwnza@gmail.com>
2025-05-20 21:13:02 +0700

    add: repo

---
+++ src/application/repositories/refresh_tokens/create.rs
@@ -0,0 +1,46 @@
+use crate::domain::errors::repositories_errors::RepositoryResult;
+use crate::infrastructure::repositories::{
+    cache_layer::applications_organization_by_client_id_repository::ApplicationsOrganizationByClientIdCacheLayerRepository,
+    cipher::{aes_gcm_repository::AesGcmCipherRepository, base64_repository::Base64Repository},
+};
+use async_trait::async_trait;
+use std::sync::Arc;
+
+pub struct CreateRefreshTokenRepositoryImpl {
+    apporg_cachelayer_repo: Arc<dyn ApplicationsOrganizationByClientIdCacheLayerRepository>,
+    base64_repo: Arc<dyn Base64Repository>,
+    aes_repo: Arc<dyn AesGcmCipherRepository>,
+}
+impl CreateRefreshTokenRepositoryImpl {
+    pub fn new(
+        apporg_cachelayer_repo: Arc<dyn ApplicationsOrganizationByClientIdCacheLayerRepository>,
+        base64_repo: Arc<dyn Base64Repository>,
+        aes_repo: Arc<dyn AesGcmCipherRepository>,
+    ) -> Self {
+        Self {
+            apporg_cachelayer_repo,
+            base64_repo,
+            aes_repo,
+        }
+    }
+}
+
+#[async_trait]
+pub trait CreateRefreshTokenRepository: Send + Sync {
+    async fn encode_refresh_token_secret(
+        &self,
+        client_key: &str,
+        encrypt_client_secret: &str,
+    ) -> RepositoryResult<String>;
+}
+
+#[async_trait]
+impl CreateRefreshTokenRepository for CreateRefreshTokenRepositoryImpl {
+    async fn encode_refresh_token_secret(
+        &self,
+        client_key: &str,
+        encrypt_client_secret: &str,
+    ) -> RepositoryResult<String> {
+        todo!()
+    }
+}
