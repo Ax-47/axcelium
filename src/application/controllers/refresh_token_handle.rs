1ff98a28933ff77abff0ed1c44c3170f1caf754a
Ax-47 <armingLwnza@gmail.com>
2025-05-22 01:17:04 +0700

    add: bring controller and service

--- src/application/controllers/refresh_token_handle.rs
+++ src/application/controllers/refresh_token_handle.rs
@@ -1,6 +1,7 @@
 use crate::application::dto::payload::refresh_token::{CreateTokenPayload, RotateTokenPayload};
 use crate::application::dto::response::refresh_token::CreateTokenResponse;
 use crate::application::services::refresh_token::create::CreateRefreshTokenService;
+use crate::application::services::refresh_token::rotate::RotateRefreshTokenService;
 use crate::domain::{
     entities::apporg_client_id::CleanAppOrgByClientId, errors::repositories_errors::ApiError,
 };
@@ -28,13 +29,14 @@ pub async fn create_refresh_token_handle(
 pub async fn rotate_refresh_token_handle(
     req: actix_web::HttpRequest,
     post_data: web::Json<RotateTokenPayload>,
-    token_service: web::Data<dyn CreateRefreshTokenService>,
+    token_service: web::Data<dyn RotateRefreshTokenService>,
 ) -> Result<web::Json<CreateTokenResponse>, ApiError> {
     let apporg = req
         .extensions()
         .get::<CleanAppOrgByClientId>()
         .ok_or_else(|| ApiError::new("Missing AppOrg data".to_string(), 500))
         .cloned()?;
+    token_service.execute(apporg, post_data.refresh_token.clone()).await?;
     // Ok(web::Json(token))
     todo!()
 }
