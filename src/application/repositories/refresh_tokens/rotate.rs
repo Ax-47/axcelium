11ad0c584d1b2c3ef9e174ab0a18d1dbe960f35d
Ax-47 <armingLwnza@gmail.com>
2025-05-22 01:22:05 +0700

    add: essensetial methods

--- src/application/repositories/refresh_tokens/rotate.rs
+++ src/application/repositories/refresh_tokens/rotate.rs
@@ -36,8 +36,106 @@ impl RotateRefreshTokenRepositoryImpl {
 
 #[async_trait]
 pub trait RotateRefreshTokenRepository: Send + Sync {
+    async fn encode_refresh_token_secret(
+        &self,
+        encrypt_client_secret: &Vec<u8>,
+    ) -> RepositoryResult<(String, String)>;
+
+    async fn genarate_token_secret(&self) -> RepositoryResult<Vec<u8>>;
+    async fn genarate_token_version_base64(&self) -> RepositoryResult<String>;
+    fn create_refresh_token(
+        &self,
+        application_id: Uuid,
+        organization_id: Uuid,
+        user_id: Uuid,
+        encrypted_token_secret: String,
+        token_version: String,
+        issued_at: OffsetDateTime,
+        expires_at: OffsetDateTime,
+    ) -> RefreshToken;
+
+    async fn create_pesato_token(
+        &self,
+        key: &Vec<u8>,
+        rt: RefreshToken,
+        secret: &str,
+        secret_key: &str,
+        issued_at: String,
+        expire: String,
+        notbefore: String,
+    ) -> RepositoryResult<String>;
+
+    fn encode_base64(&self, bytes: &Vec<u8>) -> String;
+
+    fn decode_base64(&self, plaintext: &str) -> RepositoryResult<Vec<u8>>;
+
+    async fn store_refresh_token(&self, rf: RefreshToken) -> RepositoryResult<()>;
 }
 
 #[async_trait]
 impl RotateRefreshTokenRepository for RotateRefreshTokenRepositoryImpl {
+    async fn genarate_token_secret(&self) -> RepositoryResult<Vec<u8>> {
+        let mut secret = vec![0u8; 32];
+        OsRng.try_fill_bytes(&mut secret)?;
+        Ok(secret)
+    }
+    async fn genarate_token_version_base64(&self) -> RepositoryResult<String> {
+        let mut version = vec![0u8; 24];
+        OsRng.try_fill_bytes(&mut version)?;
+        Ok(self.base64_repo.encode(&version))
+    }
+    async fn encode_refresh_token_secret(
+        &self,
+        client_secret: &Vec<u8>,
+    ) -> RepositoryResult<(String, String)> {
+        self.aes_repo.encrypt(client_secret).await
+    }
+
+    fn encode_base64(&self, bytes: &Vec<u8>) -> String {
+        self.base64_repo.encode(bytes)
+    }
+
+    fn decode_base64(&self, plaintext: &str) -> RepositoryResult<Vec<u8>> {
+        Ok(self.base64_repo.decode(plaintext)?)
+    }
+    fn create_refresh_token(
+        &self,
+        application_id: Uuid,
+        organization_id: Uuid,
+        user_id: Uuid,
+        encrypted_token_secret: String,
+        token_version: String,
+        issued_at: OffsetDateTime,
+        expires_at: OffsetDateTime,
+    ) -> RefreshToken {
+        RefreshToken {
+            token_id: Uuid::new_v4(),
+            application_id,
+            organization_id,
+            user_id,
+            encrypted_token_secret,
+            token_version,
+            parent_version: None,
+            issued_at: CqlTimestamp(issued_at.unix_timestamp()),
+            expires_at: CqlTimestamp(expires_at.unix_timestamp()),
+            revoked: false,
+        }
+    }
+    async fn create_pesato_token(
+        &self,
+        key: &Vec<u8>,
+        rt: RefreshToken,
+        secret: &str,
+        secret_key: &str,
+        issued_at: String,
+        expire: String,
+        notbefore: String,
+    ) -> RepositoryResult<String> {
+        self.paseto_repo
+            .encrypt(key, rt, secret, secret_key, issued_at, expire, notbefore)
+            .await
+    }
+    async fn store_refresh_token(&self, rf: RefreshToken) -> RepositoryResult<()> {
+        self.database_repo.create_refresh_token(rf.into()).await
+    }
 }
