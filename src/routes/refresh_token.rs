88d5d1c6a367e0c26730d17b7b09f794e0d97c20
Ax-47 <armingLwnza@gmail.com>
2025-05-22 01:13:22 +0700

    add: router

--- src/routes/refresh_token.rs
+++ src/routes/refresh_token.rs
@@ -1,4 +1,4 @@
-use crate::{application::controllers::refresh_token_handle::create_refresh_token_handle, setup::Container};
+use crate::{application::controllers::refresh_token_handle::{create_refresh_token_handle, rotate_refresh_token_handle}, setup::Container};
 use actix_web::web::{self, ServiceConfig};
 use std::sync::Arc;
 
@@ -8,7 +8,9 @@ pub fn configure(cfg: &mut ServiceConfig, container: Arc<Container>) {
     cfg.service(
         web::scope("/tokens")
             .app_data(web::Data::from(container.create_refresh_token_service.clone()))
+            .app_data(web::Data::from(container.rotate_refresh_token_service.clone()))
             .wrap(middleware)
-            .route("/", web::post().to(create_refresh_token_handle)),
+            .route("/", web::post().to(create_refresh_token_handle))
+            .route("/{token_id}/revoke", web::post().to(rotate_refresh_token_handle)),
     );
 }
