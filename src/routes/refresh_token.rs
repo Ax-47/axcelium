de6d37635d9b8dd7e3beab638f5df27d07416ba8
Ax-47 <armingLwnza@gmail.com>
2025-05-21 16:12:41 +0700

    fix: change service

--- src/routes/refresh_token.rs
+++ src/routes/refresh_token.rs
@@ -6,8 +6,8 @@ pub fn configure(cfg: &mut ServiceConfig, container: Arc<Container>) {
     let middleware = container.validate_bearer_auth_middleware_service.clone();
 
     cfg.service(
-        web::scope("/token")
-            .app_data(web::Data::from(container.create_user_service.clone()))
+        web::scope("/tokens")
+            .app_data(web::Data::from(container.create_refresh_token_service.clone()))
             .wrap(middleware)
             .route("/{user_id}", web::post().to(create_refresh_token_handle)),
     );
