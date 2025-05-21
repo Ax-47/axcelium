38fe44eda763bbe5ba914bfaa9df4e98c4dccb38
Ax-47 <armingLwnza@gmail.com>
2025-05-22 02:19:09 +0700

    add: new query

--- src/infrastructure/repositories/database/query/refresh_token.rs
+++ src/infrastructure/repositories/database/query/refresh_token.rs
@@ -1,23 +1,18 @@
-pub const INSERT_REFRESH_TOKEN :&str=r#"INSERT INTO axcelium.refresh_tokens ( 
-    token_id,
-    application_id,
-    organization_id,
-    user_id,
-    encrypted_token_secret,
-    token_version,
-    parent_version,
-    issued_at,
-    expires_at,
-    revoked
+pub const INSERT_REFRESH_TOKEN: &str = r#"
+INSERT INTO axcelium.refresh_tokens (
+    token_id, application_id, organization_id, user_id,
+    encrypted_token_secret, token_version, parent_version,
+    issued_at, expires_at, revoked
 ) VALUES (
-    :token_id,
-    :application_id,
-    :organization_id,
-    :user_id,
-    :encrypted_token_secret,
-    :token_version,
-    :parent_version,
-    :issued_at,
-    :expires_at,
-    :revoked
+    :token_id, :application_id, :organization_id,
+    :user_id, :encrypted_token_secret, :token_version,
+    :parent_version, :issued_at, :expires_at, :revoked
 ) USING TTL 2592000;"#;
+
+pub const QUERY_REFRESH_TOKEN: &str = r#"
+SELECT
+    application_id, organization_id, user_id, encrypted_token_secret,
+    token_version, parent_version, issued_at, expires_at, revoked
+FROM axcelium.refresh_tokens
+WHERE organization_id = :organization_id AND application_id = :application_id , token_id = :token_id;
+"#;
