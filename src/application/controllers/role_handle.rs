aaab9847bed3b30c3a869a158b809dab1d226636
Ax-47 <armingLwnza@gmail.com>
2025-05-23 19:36:18 +0700

    add:controller

--- src/application/controllers/role_handle.rs
+++ src/application/controllers/role_handle.rs
@@ -22,3 +22,17 @@ pub async fn create_role_handler(
     let res = role_service.execute(apporg, post_data.into_inner()).await?;
     Ok(web::Json(res))
 }
+
+pub async fn get_role_handler(
+    req: actix_web::HttpRequest,
+    role_service: web::Data<dyn CreateRoleService>,
+    post_data: web::Json<CreateRolePayload>,
+) -> Result<web::Json<SimpleResponse>> {
+    let apporg = req
+        .extensions()
+        .get::<CleanAppOrgByClientId>()
+        .ok_or_else(|| ApiError::new("Missing AppOrg data".to_string(), 500))
+        .cloned()?;
+    let res = role_service.execute(apporg, post_data.into_inner()).await?;
+    Ok(web::Json(res))
+}
