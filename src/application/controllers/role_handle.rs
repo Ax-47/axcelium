01aaff83ee43acd0cae3cf20eee739baf6942801
Ax-47 <armingLwnza@gmail.com>
2025-05-23 13:15:32 +0700

    add: controller

---
+++ src/application/controllers/role_handle.rs
@@ -0,0 +1,10 @@
+use super::super::services::hello_service::HelloService;
+use crate::application::dto::response::hello::HelloResponse;
+use actix_web::{Result, web};
+
+pub async fn create_role_handler(
+    hello_service: web::Data<dyn HelloService>,
+) -> Result<web::Json<HelloResponse>> {
+    let hello = hello_service.hello_world().await;
+    Ok(web::Json(HelloResponse { server: hello }))
+}
