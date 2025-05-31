b9010fbcb8bf33a71ce4bbbc07d63e7476c59922
Ax-47 <armingLwnza@gmail.com>
2025-05-31 23:24:02 +0700

    del: debug

--- src/infrastructure/repositories/database/scylladb.rs
+++ src/infrastructure/repositories/database/scylladb.rs
@@ -5,7 +5,6 @@ use scylla::client::session_builder::SessionBuilder;
 pub async fn get_db_pool(cfg: config::DatabaseConfig) -> Session {
     let mut builder = SessionBuilder::new();
     for url in cfg.urls {
-        println!("{url}");
         builder = builder.known_node(url);
     }
     let session = builder
