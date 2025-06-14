27223332fab8a694b4f8b3cd89a14a103bddb6ab
Ax-47 <armingLwnza@gmail.com>
2025-06-14 16:27:12 +0700

    add: replicator server and repo

--- src/application/repositories/mod.rs
+++ src/application/repositories/mod.rs
@@ -1,5 +1,6 @@
 pub mod initial_core;
-pub mod users;
-pub mod validate_bearer_auth_repository;
 pub mod refresh_tokens;
 pub mod roles;
+pub mod users;
+pub mod validate_bearer_auth_repository;
+pub mod replicator;
--- src/application/services/mod.rs
+++ src/application/services/mod.rs
@@ -1,6 +1,7 @@
 pub mod hello_service;
 pub mod initial_core_service;
-pub mod users;
-pub mod validate_bearer_auth_service;
 pub mod refresh_token;
 pub mod roles;
+pub mod users;
+pub mod validate_bearer_auth_service;
+pub mod replicator;
